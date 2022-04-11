99c8e51fe8ee64119a727317e329f6086c9dff57
aminnairi <18418459+aminnairi@users.noreply.github.com>
2022-04-11 19:34:49 +0200

    fixes

--- chapitre-15/index.js
+++ chapitre-15/index.js
@@ -1,1 +1,26 @@
-console.log("XMLHttpRequest");
+/**
+ * Le constructeur de requêtes asynchrones
+ * @see https://developer.mozilla.org/en-US/docs/Web/API/XMLHttpRequest
+ */
+const request = new XMLHttpRequest();
+
+/**
+ * Permet d'écouter l'événement permettant de savoir si la requête a bien été reçue ou non
+ * @see https://developer.mozilla.org/en-US/docs/Web/API/EventTarget
+ * @see https://developer.mozilla.org/en-US/docs/Web/API/XMLHttpRequestEventTarget
+ */
+request.addEventListener("load", function() {
+  console.log(request.responseXML);
+});
+
+/**
+ * Paramètre la requête avant son envoie avec une méthode HTTP et une URL (serveur distant ou local)
+ * @see https://developer.mozilla.org/en-US/docs/Web/HTTP/Methods
+ * @see https://developer.mozilla.org/en-US/docs/Web/API/XMLHttpRequest/open
+ */
+request.open("GET", "../chapitre-14/lol.xml");
+
+/**
+ * L'instruction permettant effectivement d'envoyer la requête asynchrone au serveur cible
+ */
+request.send();
